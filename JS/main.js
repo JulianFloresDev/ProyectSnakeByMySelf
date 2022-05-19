ed58cad4a6da5eee3b803a03f8679943e97b3e57
julian <rulodemoebius@gmail.com>
2022-05-18 23:57:56 -0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,5 @@
+let gameInterval;
+
 const bodySnake = [];
 const movement = {
     UP: 1,
@@ -28,100 +30,43 @@ let food = {
 }
 
 function startGame() {
+    gameInterval = setInterval(gameLoop, snakeChoice.velocidad);
 
     bodySnake.unshift(new SnakeCell(250, 20, movement.DOWN));
     bodySnake.push(new SnakeCell(250, 10, movement.DOWN));
     bodySnake.push(new SnakeCell(250, 0, movement.DOWN));
 }
 
+//__________________________________________________________________________________________________________________________________________//
 
 // Ingreso de datos para la seleccion de la serpiente a utilizar.
+const score = document.querySelector(".score");
 
-let userNameInput = prompt("Bienvenido al Snake Game NFT!!!! \n¿Como quieres que se vea tu record en la tabla mundial?");
-let userSnakeChoice = prompt("Perfecto " + userNameInput + " bienvenido al 'Snake Game NFT'!\nAntes de iniciar el juego deberás elegir una 'Snake' desde tu portafolio de NFTs, o comprar una si aún no tienes ninguna!\nEscoge la opción de tu preferencia a continuación:\n\n1 - Seleccionar desde Portafolio.\n2 - Comprar una nueva desde Marketplace.");
+const startButton = document.querySelector("#btn-startGame");
 
-let snakeChoice = new Snake("#484848", 100, 3, false, false, "#484848"); //default snake.
+const startOptions = document.querySelector(".btn-start");
 
-let coincidence = false;
-while (coincidence == false) {
-    for (int = 0; int < 2; int++) {
-        if (userSnakeChoice == '1') {
-            selecUserSnake();
-            coincidence = true;
-            break;
-        } else if (userSnakeChoice == '2') {
-            buyNewSnake();
-            coincidence = true;
-            break;
-        } else {
-            userSnakeChoice = prompt(
-                `Perfecto ${userNameInput} bienvenido al 'Snake Game NFT'!
-                Antes de iniciar el juego deberás elegir una 'Snake' desde tu portafolio de NFTs, o comprar una si aún no tienes ninguna!
-                Escoge la opción de tu preferencia a continuación:
-                
-                1 - Seleccionar desde Portafolio.
-                2 - Comprar una nueva desde Marketplace.
-            `);
-        }
-    }
-    if (int == 2) {
-        alert(
-            `Lo siento ${userNameInput} se agotaron los intentos para seleccionar una opción.
-            Intente jugar mañana!
-        `)
-        coincidence = true;
-    }
-}
-
-function selecUserSnake() {
-    let colorSnake = prompt(
-        `Hola de nuevo ${userNameInput} selecciona una de tus 'Snake' de tu portafolio personal:
-    
-        0 - Comprar otra 'Snake' nueva.
-        1 - Snake Red
-        2 - Snake Black 
-        3 - Snake Blue 
-        4 - Snake Green 
-    `);
-
-    switch (colorSnake) {
-        case '0':
-            buyNewSnake();
-            break;
-        case '1':
-            snakeChoice = new Snake("#D94945", 90, 3, true, false, "#D94945");
-            startGame();
-            break;
-        case '2':
-            snakeChoice = new Snake("#0F0E0E", 120, 3, true, false, "#DCE85B");
-            startGame();
-            break;
-        case '3':
-            snakeChoice = new Snake("#4D86B6", 60, 3, true, false, "#0F0E0E");
-            startGame();
-            break;
-        case '4':
-            snakeChoice = new Snake("#2A662B", (1000 / 15), 3, true, false, "#79820B");
-            startGame();
-            break;
-        default:
-            snakeChoice = new Snake("#484848", 75, 3, false, false, "#484848");
-            startGame();
-            alert("Bueno puede ingresar igualmente y jugar con el prediseñado.\n\nSu score no se guardará.");
-            intentos = 4;
-    }
-    return snakeChoice
-}
-
-function buyNewSnake() {
-
-    snakeChoice = new Snake("#484848", 180, 3, false, false, "#484848");
-    alert('Por le momento esta sección no está disponible.\nIgualmente podrá jugar con una versión clásica.\n\nMucha Suerte!!')
+startButton.addEventListener('click', () => {
+    startOptions.style.display = 'none';
+    //En new los datos de la serpiente tienen que ser los ingresados por el panel lateral que tengo que agregar.
 
     startGame();
+});
 
-    return snakeChoice
-}
+
+snakeChoice = new Snake("#0F0E0E", 120, 3, true, false, "#DCE85B");
+
+
+
+//snakeChoice = new Snake("#D94945", 90, 3, true, false, "#D94945")
+//snakeChoice = new Snake("#0F0E0E", 120, 3, true, false, "#DCE85B");
+
+//snakeChoice = new Snake("#4D86B6", 60, 3, true, false, "#0F0E0E");
+
+//snakeChoice = new Snake("#2A662B", (1000 / 15), 3, true, false, "#79820B");
+
+//snakeChoice = new Snake("#484848", 75, 3, false, false, "#484848");
+
 
 
 //Hasta acá el código corresponde a la identificación del usuario y el ingreso de su 'Snake'
@@ -245,6 +190,25 @@ const ajustPosition = () => {
 
 //Condiciones de finalización y continuidad del juego.
 
+const ajustScore = () => {
+    let result = (snakeChoice.lenghtSnake - 3);
+
+    let h3 = document.querySelector(".score h3")
+    h3.remove();
+
+    let newScore = document.createElement("h3");
+    newScore.innerHTML = `Score: ${result}`;
+    score.appendChild(newScore);
+}
+
+const endGame = () => {
+    ctx.font = "30px Press Start";
+    ctx.fillText("Game over!!", 190, 250);
+    ctx.fillText(`Score: ${snakeChoice.lenghtSnake - 3}`, 210, 300);
+
+    clearInterval(gameInterval);
+}
+
 const checkPosition = () => {
     //Verificamos que la cabeza de la serpiente no se encuentre en superposición con el resto del cuerpo
     for (i = 0; i < bodySnake.length; i++) {
@@ -252,9 +216,7 @@ const checkPosition = () => {
             if (bodySnake[i] != bodySnake[j]) {
                 if (bodySnake[i].posX == bodySnake[j].posX && bodySnake[i].posY == bodySnake[j].posY) {
 
-                    ctx.font = '38px Press Start 2P';
-                    ctx.fillText("Game over!!", 230, 250);
-                    clearInterval(gameInterval);
+                    endGame();
 
                     console.log("¡¡Game Over!! Press F5 for play again");
                 }
@@ -263,10 +225,9 @@ const checkPosition = () => {
     }
     //Verificamos que la cabeza de la serpiente no se encuentre fuera del margen del juego delimitado por esas posiciones
     if (bodySnake[0].posX < 10 || bodySnake[0].posX + 10 > 490 || bodySnake[0].posY < 10 || bodySnake[0].posY + 10 > 490) {
-        ctx.font = "30px Press Start";
-        ctx.fillText("Game over!!", 190, 250);
-        clearInterval(gameInterval);
 
+        endGame();
+        
         console.log("¡¡Game Over!! Press F5 for play again");
     }
     //Verificamos si la cabeza de la serpiente coincide con la posicion de la comida; en caso de que coincidan aumentamos la longitud de la serpiente, y redefinimos una nueva posicion para la serpiente
@@ -276,8 +237,7 @@ const checkPosition = () => {
         food.x = Math.round(Math.random() * 47 + 1) * (10);
         food.y = Math.round(Math.random() * 47 + 1) * (10);
         drawFood();
-        console.log(userNameInput + " score: " + (snakeChoice.lenghtSnake - 3));
-
+        ajustScore();
     }
 }
 
@@ -293,7 +253,6 @@ function gameLoop() {
     drawSnake();
 }
 
-let gameInterval = setInterval(gameLoop, snakeChoice.velocidad);
 
 console.log(snakeChoice);
 
@@ -306,3 +265,5 @@ console.log(snakeChoice);
 //Appdelante: Daniel Macario: https://www.youtube.com/watch?v=GbPAYZ7tXUY
 
 //Juego de AppDelante: https://culebrita-appdelante.vercel.app/juego.html
+
+//__________________________________________________________________________________________________________________________________________//
