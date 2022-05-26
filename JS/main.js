2fd4cd76a7beeb70505995f2f6cf6a7419bc8907
julian <rulodemoebius@gmail.com>
2022-05-26 13:25:28 -0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,178 @@
+//Constantes para la manipulación del DOM
+//__________________________________________________________________________________________________________________________________________//
+
+const headerElementsBtn = document.querySelectorAll(".headerElements"),
+
+    gameConteiner = document.querySelector(".conteiner"),
+
+    selectSnakeBtn = document.querySelectorAll(".btn-selectSnake"),
+
+    buySnakeBtn = document.querySelector(".btn-buySnake"),
+
+    scoreBtn = document.querySelector(".btn-showScore"),
+
+    divSnakeValues = document.querySelector(".snake-values"),
+
+    divScoreTable = document.querySelector(".globalScore"),
+
+    score = document.querySelector(".score"),
+
+    singInBtn = document.querySelector(".btn-singIn"),
+
+    divInvalid = document.querySelector(".invalid"),
+
+    startPlayBtn = document.querySelector(".btn-startGame"),
+
+    logOutBtn = document.querySelector(".btn-logOut"),
+
+    sesionStartOptions = document.querySelector(".sesionStart"),
+
+    inputUsername = document.querySelector("#usernameInput"),
+
+    inputPassword = document.querySelector("#passwordInput"),
+
+
+    displayStartOptions = () => {
+        gameConteiner.style.display = 'flex';
+        logOutBtn.style.display = "flex";
+    };
+
+
+//__________________________________________________________________________________________________________________________________________//
+
+//                                       Inicio de Sesión y Registro del Usuario
+//__________________________________________________________________________________________________________________________________________//
+const userRegistration = [];
+
+class Player {
+    constructor(usernameLogIn, passwordLogIn, cardRegistered, snakeColecction) {
+        this.usernameLogIn = usernameLogIn;
+        this.passwordLogIn = passwordLogIn;
+        this.cardRegistered = cardRegistered;
+        this.snakeColecction = snakeColecction;
+    }
+};
+
+singInBtn.addEventListener("click", () => {
+    sesionStartOptions.style.display = "flex";
+    gameConteiner.style.display = "none";
+    logOutBtn.style.display = "none";
+});
+
+const displayGame = () =>{
+    sesionStartOptions.style.display = "none";
+    singInBtn.style.display = "none";
+    logOutBtn.style.display = "flex";
+    gameConteiner.style.display = "flex";
+}
+
+const userInputVerification = () => {
+    if (inputUsername.value == "" || inputPassword.value == "") {
+
+        console.log(inputUsername, inputPassword);
+
+        divInvalid.innerHTML = `
+            <p>
+                You must enter a valid name and password.
+            </p>`;
+
+    } else {
+        displayGame();
+    }
+};
+
+
+startPlayBtn.addEventListener("click", () => {
+    userInputVerification();
+    //Guardar ingreso del usuario y ejecutar funcion de comprobacion de datos
+})
+
+for (const btn of headerElementsBtn) {
+    btn.addEventListener("click", () => {
+        if (condition) {
+            
+        } else {
+            
+        }
+    })
+}
+
+//__________________________________________________________________________________________________________________________________________//
+
+
+// Ingreso de datos para la seleccion de la serpiente a utilizar.
+
+const snakeColecction = [];
+
+//Para cada boton de los seleccionados con querySelectorAll le damos la opción de ocultar las opciones iniciales y mostrar el div de selección.
+for (const btn of selectSnakeBtn) {
+    btn.addEventListener("click", () => {
+
+        //Agregar un cuerpo al body que permita seleccionar una snake dentro de las que el usuario tenga
+
+    })
+};
+
+//El siguiente bloque muestra y oculta el "Global Score".
+let btnScoreClicked = false;
+scoreBtn.addEventListener("click", () => {
+    switch (btnScoreClicked) {
+        case false:
+            divScoreTable.style.display = "flex";
+            btnScoreClicked = true;
+            break;
+        case true:
+            divScoreTable.style.display = "none";
+            btnScoreClicked = false;
+            break;
+    }
+});
+
+
+
+//Para cada boton de los seleccionados con querySelectorAll le damos la opción de ocultar las opciones iniciales y mostrar
+// for (const btn of startButton) {
+//     btn.addEventListener('click', () => {
+
+//         userInputVerification();
+
+//         sesionStartOptions.style.display = 'none';
+//         singInBtn.style.display = 'none';
+
+//         score.innerHTML = `<h3>Score: 0</h3>`;
+//         clearCanva();
+
+//         displayStartOptions();
+
+//         //En new los datos de la serpiente tienen que ser los ingresados por el panel lateral que tengo que agregar.
+
+//         
+//     });
+// };
+
+
+
+//snakeChoice = new Snake("#D94945", 90, 3, true, false, "#D94945");    Roja
+//snakeChoice = new Snake("#0F0E0E", 120, 3, true, false, "#DCE85B");   Negra
+
+//snakeChoice = new Snake("#4D86B6", 60, 3, true, false, "#0F0E0E");    Azul
+
+//snakeChoice = new Snake("#2A662B", (1000 / 15), 3, true, false, "#79820B");   Verde
+
+//snakeChoice = new Snake("#484848", 75, 3, false, false, "#484848");   Gris
+
+
+
+
+//Hasta acá el código corresponde a la identificación del usuario y el ingreso de su 'Snake'
+//__________________________________________________________________________________________________________________________________________//
+
+const saveNewData = (key, value) => {
+    localStorage.setItem(key, value);
+}
+// saveNewData("snakeBodyPosition", JSON.stringify(bodySnake));
+
+
 let gameInterval;
 
 const bodySnake = [];
@@ -8,7 +183,8 @@ const movement = {
     RIGHT: 4,
 }
 class Snake {
-    constructor(color, userSpeed, lenghtSnake, usuarioSnake, skin, secondaryColor) {
+    constructor(name, color, userSpeed, lenghtSnake, usuarioSnake, skin, secondaryColor) {
+        this.name = name;
         this.color = color;
         this.speed = userSpeed;
         this.lenghtSnake = lenghtSnake;
@@ -29,13 +205,14 @@ let food = {
     y: Math.round(Math.random() * 47 + 1) * (10),
 }
 
-snakeChoice = new Snake("#0F0E0E", 105, 4, true, false, "#DCE85B");
+snakeChoice = new Snake("Black Piton", "#0F0E0E", 105, 4, true, false, "#DCE85B");
 
 
 function startGame() {
+    food.x = Math.round(Math.random() * 47 + 1) * (10),
+        food.y = Math.round(Math.random() * 47 + 1) * (10),
 
-    clearCanva();
-    clearInterval(gameInterval);
+        clearCanva();
     if (bodySnake.length > 0) {
         let maxCells = bodySnake.length;
         for (let i = 0; i < maxCells; i++) {
@@ -43,82 +220,16 @@ function startGame() {
         }
     }
 
+    clearInterval(gameInterval);
     gameInterval = setInterval(gameLoop, snakeChoice.speed);
 
     bodySnake.unshift(new SnakeCell((snakeChoice.lenghtSnake * 10), 10, movement.RIGHT));
     for (let i = 1; i < snakeChoice.lenghtSnake; i++) {
         bodySnake.push(new SnakeCell((snakeChoice.lenghtSnake - i) * 10, 10, movement.RIGHT));
     }
-    // bodySnake.push(new SnakeCell(20, 10, movement.RIGHT));
-    // bodySnake.push(new SnakeCell(10, 10, movement.RIGHT));
+    // saveNewData("snakeBodyPosition", JSON.stringify(bodySnake)); Acá llamaría a la función "saveNewData" Para definir que Snake fué seleccionada dentro de las snakes del usuario
+
 }
-
-//__________________________________________________________________________________________________________________________________________//
-
-// Ingreso de datos para la seleccion de la serpiente a utilizar.
-
-const selectSnakeBtn = document.querySelector(".btn-selectSnake"),
-
-buySnakeBtn = document.querySelector(".btn-buySnake"),
-
-startButton = document.querySelectorAll(".btn-startGame"),
-
-scoreBtn = document.querySelector(".btn-showScore"),
-
-divSnakeValues = document.querySelector(".snake-values"),
-
-startOptions = document.querySelector(".btn-start"),
-
-divScoreTable = document.querySelector(".globalScore"),
-
-score = document.querySelector(".score");
-
-selectSnakeBtn.addEventListener("click", () => {
-    clearCanva();
-    divSnakeValues.style.display = "flex";
-    startOptions.style.display = 'none';
-
-})
-
-let btnScoreClicked = false;
-scoreBtn.addEventListener("click", () => {
-    switch (btnScoreClicked) {
-        case false: 
-            divScoreTable.style.display = "flex";
-            btnScoreClicked = true;
-            break;
-        case true:
-            divScoreTable.style.display = "none";
-            btnScoreClicked = false;
-            break;
-    }
-})
-
-for (const btn of startButton) {
-
-    btn.addEventListener('click', () => {
-        clearCanva();
-        startOptions.style.display = 'none';
-        divSnakeValues.style.display = 'none';
-        //En new los datos de la serpiente tienen que ser los ingresados por el panel lateral que tengo que agregar.
-
-        drawInstructions();
-    });
-}
-
-
-//snakeChoice = new Snake("#D94945", 90, 3, true, false, "#D94945");    Roja
-//snakeChoice = new Snake("#0F0E0E", 120, 3, true, false, "#DCE85B");   Negra
-
-//snakeChoice = new Snake("#4D86B6", 60, 3, true, false, "#0F0E0E");    Azul
-
-//snakeChoice = new Snake("#2A662B", (1000 / 15), 3, true, false, "#79820B");   Verde
-
-//snakeChoice = new Snake("#484848", 75, 3, false, false, "#484848");   Gris
-
-
-
-//Hasta acá el código corresponde a la identificación del usuario y el ingreso de su 'Snake'
 
 //__________________________________________________________________________________________________________________________________________//
 
@@ -146,7 +257,11 @@ const drawInstructions = () => {
     ctx.fillText(`Hello Human!! Welcome to Snake Game...`, 90, 100);
     ctx.fillText(`Use arrow's key to move the Snake`, 110, 140);
     ctx.fillText(`Press 'intro' to Start...`, 170, 270);
+    ctx.fillText(`Sing In or Registrate for get Full Acces`, 90, 330);
+    ctx.fillText(`to Marketplace and Diferents Snakes`, 95, 360);
 }
+
+drawInstructions();
 
 let clearCanva = () => {
     ctx.clearRect(0, 0, 500, 500);
@@ -175,7 +290,7 @@ const drawBox = (xi, yi, color, secondaryColor) => {
 }
 
 const drawFood = () => {
-    drawBox(food.x, food.y, "#254925", "#254925");
+    drawBox(food.x, food.y, "#BC170C", "#EF8E69");
 
     let empySlot = true;
     for (i = 0; i < bodySnake.length; i++) {
@@ -187,7 +302,7 @@ const drawFood = () => {
         }
     }
     if (empySlot == true) {
-        drawBox(food.x, food.y, "#254925", "#254925");
+        drawBox(food.x, food.y, "#BC170C", "#EF8E69");
     } else drawFood();
 }
 
@@ -211,9 +326,10 @@ const movementKey = (e) => {
     } else if (e.code === "ArrowRight" && bodySnake[0].vectorMov !== movement.LEFT) {
         bodySnake[0].vectorMov = movement.RIGHT;
     } else if (e.code === "Enter") {
-        startOptions.style.display = 'none';
-        divSnakeValues.style.display = 'none';
-        startGame();
+        if(gameConteiner.style.display != "none"){
+
+            startGame();
+        }
     } else return;
 }
 document.addEventListener("keydown", movementKey);
@@ -269,9 +385,12 @@ const endGame = () => {
 
     clearInterval(gameInterval);
 
-    startOptions.style.display = 'flex';
+    saveNewData("userLocalScore", JSON.stringify())
+
+    score.innerHTML = `<h3>Score: 0</h3>`
 
     if (snakeChoice.owner == "true") {
+        //Agregar puntaje a la tabla de las mejores posiciones!
 
     }
 }
